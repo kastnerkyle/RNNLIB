f0942c86827d9b99fae23fee92f721a72af0bdd3
Kyle Kastner <kastnerkyle@gmail.com>
2014-03-15 02:43:33 -0500

    Added get_prereqs code for Ubuntu

---
+++ get_prereqs_ubuntu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Kyle Kastner, March 2014
+
+sudo apt-get install -y gcc
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y autoconf automake autotools-dev
+sudo apt-get install -y libnetcdf-dev
