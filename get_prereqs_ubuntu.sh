9ac9e025835e2d3d5c771a521758010c98382f4e
Kyle Kastner <kastnerkyle@gmail.com>
2014-03-15 10:58:53 -0500

    Added small changes to prereqs file

--- get_prereqs_ubuntu.sh
+++ get_prereqs_ubuntu.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 #Kyle Kastner, March 2014
-
 sudo apt-get install -y gcc
 sudo apt-get install -y libboost1.46-dev
 sudo apt-get install -y autoconf automake autotools-dev
 sudo apt-get install -y libnetcdf-dev
+
+#this project support doxygen for docs
+#sudo apt-get install doxygen
+
+#you may need to install netcdf4
+#pip install netcdf4
+
