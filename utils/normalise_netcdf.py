c89c8afc12538aff2159b1264562e64ad642f847
Kyle Kastner <kastnerkyle@gmail.com>
2014-03-15 06:25:53 -0500

    Added netcdf4 support to normalise_netcdf.py

--- utils/normalise_netcdf.py
+++ utils/normalise_netcdf.py
@@ -1,17 +1,17 @@
 #Copyright 2009,2010 Alex Graves
 #
 # This file is part of RNNLIB.
-# 
+#
 # RNNLIB is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # RNNLIB is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with RNNLIB.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -83,7 +83,7 @@ for p in range(len(inputStds)):
         else:
             outputArray[:,p]=(outputArray[:,p]-inputMeans[p])/inputStds[p]
 
-outfile = netcdf_helpers.NetCDFFile(outputFilename, 'w')
+outfile = netcdf_helpers.netCDF4.Dataset(outputFilename, 'w')
 
 for d in inputVar.dimensions:
     netcdf_helpers.createNcDim(outfile,d,infile.dimensions[d])
